026ebd4e757612f2a1c7e7b382b335ea1b2e2f1e
vdotup <v.up@live.com>
2021-11-03 01:18:39 +0300

    Initial Commit

---
+++ WaterSortSolver/WaterSortSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterSortSolverApp.swift
+//  WaterSortSolver
+//
+//  Created by Abdurrahman Alfudeghi on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WaterSortSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
